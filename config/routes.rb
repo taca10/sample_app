2b35d439a8e0f54a8b7ead337cef2b0c864a49c2
taca10 <taka.ur.0505@gmail.com>
2018-04-22 14:25:01 +0900

    chapter3.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/bar'
+
+  get 'static_pages/baz'
+
   get 'static_pages/home'
-
   get 'static_pages/help'
-
+  get 'static_pages/about'
   root 'application#hello'
 end
