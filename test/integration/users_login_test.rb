fab50eaae36a60d5ce82eb5f13fab9f121ea36f2
taca10 <taka.ur.0505@gmail.com>
2018-05-07 00:04:37 +0900

    chapter9

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -26,12 +26,32 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+  
+  test "login without remembering" do
+    # クッキーを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    # クッキーを削除してログイン
+    # log_in_as(@user, remember_me: '0')
+    # assert_empty cookies['remember_token']
   end
 end
