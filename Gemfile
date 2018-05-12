0b5fea05793f1af1cf5e19f6835b753079a26d54
taca10 <taka.ur.0505@gmail.com>
2018-05-12 18:27:26 +0900

    chapter10.4.1

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails', '~> 4.3.1'
 gem 'faker',          '>= 1.7.3'
+gem 'will_paginate',           '3.1.6'
+gem 'bootstrap-will_paginate', '1.0.0'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,23 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user       = users(:michael)
+    @other_user = users(:archer)
+  end
+
   test "should get new" do
     get users_new_url
     assert_response :success
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password: @other_user,
+                                            password_confirmation: @other_user,
+                                            admin: true } }
+    assert_not @other_user.reload.admin?
+  end
 end
