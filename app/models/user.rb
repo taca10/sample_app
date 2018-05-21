834d786919a72dc97a720c13385974da903d58f8
taca10 <taka.ur.0505@gmail.com>
2018-05-21 18:05:57 +0900

    chapter14

--- app/models/user.rb
+++ app/models/user.rb
@@ -81,7 +81,10 @@ class User < ApplicationRecord
   # 試作feedの定義
   # 完全な実装は次章の「ユーザーをフォローする」を参照
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   #ユーザーをフォローする
